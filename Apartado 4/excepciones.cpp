e09a5314907fbbf35003fccb577e59fc8648d2a2
maritoo17 <msancgim@myuax.com>
2023-12-14 13:07:34 +0100

    Segunda parte del código en la que añado la función ExcepciónAsistencia.

--- Apartado 4/excepciones.cpp
+++ Apartado 4/excepciones.cpp
@@ -2,3 +2,9 @@
 #include <exception>
 
 using namespace std;
+
+struct ExcepcionAsistencia : public exception {
+    const char* what() const throw() override {
+        return "Error: Problema con la asistencia.";
+    }
+};

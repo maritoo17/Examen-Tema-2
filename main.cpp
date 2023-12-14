c3951a88c6f8c602527e14417975d9eb238537c2
maritoo17 <msancgim@myuax.com>
2023-12-14 11:51:07 +0100

    Cuarta parte del ejercicio en la que añado la función main en la que doy los valores que quiero a las incógnitas correspondientes.

--- main.cpp
+++ main.cpp
@@ -13,3 +13,14 @@ mostrarEstudiante(Estudiante estudiante) {
     cout << "Edad: " << estudiante.edad << endl;
     cout << "Promedio: " << estudiante.promedio << endl;
 }
+
+int main() {
+    Estudiante estudiante1;
+    estudiante1.nombre = "Juan";
+    estudiante1.edad = 20;
+    estudiante1.promedio = 9.5;
+
+    mostrarEstudiante(estudiante1);
+
+    return 0;
+}

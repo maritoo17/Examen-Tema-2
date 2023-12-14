60af5b1dd95ea7f3a6a407a150ad298161fb6b23
maritoo17 <msancgim@myuax.com>
2023-12-14 12:01:12 +0100

    Tercera parte del ejercicio en la que añado la función mostrarEstudiante tal y como nos piden en el primera apartado.

--- Apartado 1/estructura(c).cpp
+++ Apartado 1/estructura(c).cpp
@@ -5,3 +5,9 @@ struct Estudiante {
     int edad;
     float promedio;
 };
+
+void mostrarEstudiante(struct Estudiante estudiante) {
+    printf("Nombre: %s\n", estudiante.nombre);
+    printf("Edad: %d\n", estudiante.edad);
+    printf("Promedio: %.2f\n", estudiante.promedio);
+}

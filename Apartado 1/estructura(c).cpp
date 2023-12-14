adda19649a0751bd678180144a6713502deaa6ef
maritoo17 <msancgim@myuax.com>
2023-12-14 12:05:58 +0100

    Última parte del apartado en la que añado la función main que será la encargada de dar los valores para la función y la librería necesaria para que la función strcpy funcione.

--- Apartado 1/estructura(c).cpp
+++ Apartado 1/estructura(c).cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 struct Estudiante {
     char nombre[30];
@@ -10,4 +11,15 @@ void mostrarEstudiante(struct Estudiante estudiante) {
     printf("Nombre: %s\n", estudiante.nombre);
     printf("Edad: %d\n", estudiante.edad);
     printf("Promedio: %.2f\n", estudiante.promedio);
+}
+
+int main() {
+    struct Estudiante estudiante1;
+    strcpy(estudiante1.nombre,"Juan");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 9.5;
+
+    mostrarEstudiante (estudiante1);
+
+    return 0;
 }
